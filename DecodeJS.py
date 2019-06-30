2f53428591bf09f7c77dd7c555b4d1504afa011f
Webber <51797309+himanshusriGithub@users.noreply.github.com>
2019-06-30 10:40:28 +0530

    Added comment to the program

--- DecodeJS.py
+++ DecodeJS.py
@@ -1,14 +1,29 @@
 """
-Developer:
+Developer:Himanshu
 Version:0.1
-Date:
-Description: what it does in details
-
+Date:29/06/19
+Description: Decode the given Matrix Script
+Problem Statement: https://www.hackerrank.com/challenges/matrix-script/problem
  """
-# Why import
+# re is Regular Expression,in which it import the library re from regular expression.
+
 import re
-# Input ...details
+
+
+# p is row number and q is column number where p,q takes input from user.
+
+print("Enter number of row and column  and then enter values in next line ")
 p, q = map(int, input().split())
 
+# In this interpreter reads all values from columnwise and arrange the value in  rowwise.
+#  'va' is reading values from given range of row and then arranges values
+#  through 'zip'.join is used to join all values of 'va'.
+
 result=''.join([''.join(va) for va in zip(*[input() for va in range(p)])])
+
+# 're' is regular expression which imported from re.
+# '\b' is start boundary
+# ^a-zA-Z0-9 means that it will takes only starting value a-zA-Z0-9
+# and if value except these then it will print whitespace.
+
 print(re.sub(r'\b[^a-zA-Z0-9]+\b', ' ', result))
